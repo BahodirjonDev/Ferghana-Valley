6c90c1ed69454c326cc1c9d88d1cc42460e8977c
BahodirjonDev <webdev0301@gmail.com>
2021-12-04 16:00:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var options = {
+  strings: ['Ferghana Valley', 'Ferghana Valley = Silicon Valley'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.nav', options);
+
+
+var options = {
+    strings: ['The Fergana Valley competes with silicon   ', 'The Fergana Valley will be an IT ecosystem '],
+    typeSpeed: 40
+  };
+  
+  var typed = new Typed('.nav2', options);
